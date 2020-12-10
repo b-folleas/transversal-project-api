721c0106675042e65b227d30d5dc15d4e76da667
Eloi Bellet <eloi.bellet@cpe.fr>
2020-12-10 11:02:13 +0100

    fix datatest with good mapItems data

--- src/main/java/com/projettransversal/api/Helpers/DataTest.java
+++ src/main/java/com/projettransversal/api/Helpers/DataTest.java
@@ -1,9 +1,9 @@
 package com.projettransversal.api.Helpers;
 
-import com.projettransversal.api.Models.Barrack;
-import com.projettransversal.api.Models.Incident;
-import com.projettransversal.api.Models.Truck;
+import com.projettransversal.api.Models.*;
+import com.projettransversal.api.Repositories.MapItemRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTest {
@@ -11,38 +11,40 @@ public class DataTest {
     public List<Barrack> barracks;
     public List<Truck> trucks;
 
-    public DataTest() {
-        /*incidents = new ArrayList<Incident>() {
+    public DataTest(MapItemRepository mapItemRepository) {
+        List<MapItem> mapItems = (List<MapItem>) mapItemRepository.findAll();
+
+        incidents = new ArrayList<Incident>() {
             {
                 add(new Incident(mapItems.get(0), 5, IncidentType.FIRE));
                 add(new Incident(mapItems.get(1),  9, IncidentType.FIRE));
                 add(new Incident(mapItems.get(2), 7, IncidentType.FIRE));
                 add(new Incident(mapItems.get(3), 1, IncidentType.FIRE));
                 add(new Incident(mapItems.get(4), 2, IncidentType.FIRE));
-                add(new Incident(mapItems.get(4), 3, IncidentType.FIRE));
+                add(new Incident(mapItems.get(5), 3, IncidentType.FIRE));
             }
         };
 
         barracks = new ArrayList<Barrack>() {
             {
-                add(new Barrack(mapItems.get(0)));
-                add(new Barrack(mapItems.get(1)));
-                add(new Barrack(mapItems.get(2)));
-                add(new Barrack(mapItems.get(3)));
-                add(new Barrack(mapItems.get(4)));
-                add(new Barrack(mapItems.get(5)));
+                add(new Barrack(mapItems.get(6)));
+                add(new Barrack(mapItems.get(7)));
+                add(new Barrack(mapItems.get(8)));
+                add(new Barrack(mapItems.get(9)));
+                add(new Barrack(mapItems.get(10)));
+                add(new Barrack(mapItems.get(11)));
             }
         };
 
         trucks = new ArrayList<Truck>() {
             {
-                add(new Truck(mapItems.get(0), true, barracks.get(0)));
-                add(new Truck(mapItems.get(1), true, barracks.get(1)));
-                add(new Truck(mapItems.get(2), true, barracks.get(2)));
-                add(new Truck(mapItems.get(3), true, barracks.get(3)));
-                add(new Truck(mapItems.get(4), true, barracks.get(4)));
-                add(new Truck(mapItems.get(5), true, barracks.get(5)));
+                add(new Truck(mapItems.get(12), true, barracks.get(0)));
+                add(new Truck(mapItems.get(13), true, barracks.get(1)));
+                add(new Truck(mapItems.get(14), true, barracks.get(2)));
+                add(new Truck(mapItems.get(15), true, barracks.get(3)));
+                add(new Truck(mapItems.get(16), true, barracks.get(4)));
+                add(new Truck(mapItems.get(17), true, barracks.get(5)));
             }
-        };*/
+        };
     }
 }
--- src/main/java/com/projettransversal/api/Helpers/Seeder.java
+++ src/main/java/com/projettransversal/api/Helpers/Seeder.java
@@ -5,6 +5,7 @@ import com.projettransversal.api.Models.Incident;
 import com.projettransversal.api.Models.Truck;
 import com.projettransversal.api.Repositories.BarrackRepository;
 import com.projettransversal.api.Repositories.IncidentRepository;
+import com.projettransversal.api.Repositories.MapItemRepository;
 import com.projettransversal.api.Repositories.TruckRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,13 +15,16 @@ import java.util.List;
 @Component
 public class Seeder implements CommandLineRunner {
 
+    MapItemRepository _mapItemRepository;
     TruckRepository _truckRepository;
     IncidentRepository _incidentRepository;
     BarrackRepository _barrackRepository;
 
-    public Seeder(TruckRepository truckRepository,
+    public Seeder(MapItemRepository mapItemRepository,
+                  TruckRepository truckRepository,
                   IncidentRepository incidentRepository,
                   BarrackRepository barrackRepository) {
+        this._mapItemRepository = mapItemRepository;
         this._truckRepository = truckRepository;
         this._incidentRepository = incidentRepository;
         this._barrackRepository = barrackRepository;
@@ -28,11 +32,12 @@ public class Seeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        DataTest data = new DataTest();
+        DataTest data = new DataTest(_mapItemRepository);
         loadIncidents(data.incidents);
         loadBarracks(data.barracks);
         loadTrucks(data.trucks);
     }
+
 
     private void loadIncidents(List<Incident> data) {
         if (_incidentRepository.count() == 0) {
