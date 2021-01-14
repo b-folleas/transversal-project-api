82e7b079433c7a77d37e239fc477a70bbbd16c89
Nathan Mittelette <nathan.mittelette@axopen.com>
2021-01-14 15:56:46 +0100

    Ajout de swagger

--- src/main/java/com/projettransversal/api/Controllers/IncidentController.java
+++ src/main/java/com/projettransversal/api/Controllers/IncidentController.java
@@ -3,6 +3,7 @@ package com.projettransversal.api.Controllers;
 import com.projettransversal.api.Models.Incident;
 import com.projettransversal.api.Models.ViewModels.IncidentViewModel;
 import com.projettransversal.api.Services.IServices.IIncidentService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,25 @@ public class IncidentController {
     }
 
     @RequestMapping(value = "/incidents", method = RequestMethod.GET)
+    @ApiOperation(value = "Récupération de tous les incidents.")
     public List<Incident> getIncidents() {
         return _incidentService.findAll();
     }
 
     @RequestMapping(value = "/incident/{id}", method = RequestMethod.GET)
+    @ApiOperation(value = "Récupération d'un incident en fonction de son ID.")
     public Optional<Incident> getIncidentById(@PathVariable Long id) {
         return _incidentService.findById(id);
     }
 
     @RequestMapping(value = "/incident", method = RequestMethod.POST)
+    @ApiOperation(value = "Création d'un incident à travers l'objet IncidentViewModel.")
     public Incident getIncidentById(@RequestBody IncidentViewModel incidentVM) {
         return _incidentService.create(incidentVM);
     }
 
     @RequestMapping(value = "/incident/{incident_id}/intensity/{new_intensity}", method = RequestMethod.POST)
+    @ApiOperation(value = "Modification de l'intensité d'un incident à travers son ID.")
     public Incident updateIncident(@PathVariable Long incident_id, @PathVariable int new_intensity) {
         return this._incidentService.updateIncidentIntensity(incident_id, new_intensity);
     }
--- src/main/java/com/projettransversal/api/Controllers/MapItemController.java
+++ src/main/java/com/projettransversal/api/Controllers/MapItemController.java
@@ -2,6 +2,8 @@ package com.projettransversal.api.Controllers;
 
 import com.projettransversal.api.Models.MapItem;
 import com.projettransversal.api.Services.IServices.IMapItemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,11 +22,13 @@ public class MapItemController {
     }
 
     @RequestMapping(value = "/mapItems", method = RequestMethod.GET)
+    @ApiOperation(value = "Récupération de tous les mapItems.")
     public List<MapItem> getMapItems() {
         return _truckService.findAll();
     }
 
     @RequestMapping(value = "/mapItem/{id}", method = RequestMethod.GET)
+    @ApiOperation(value = "Récupération d'un mapItemp en fonction de son ID.")
     public Optional<MapItem> getMapItemById(@PathVariable Long id) {
         return _truckService.findById(id);
     }
